d54abef024d28e14345cfbf3a2f23ae4bd33e0f8
Guizin997 <guiguirussas@gmail.com>
2024-06-18 06:38:58 -0300

    cadastro de carros completo

--- site/cars_table.php
+++ site/cars_table.php
@@ -1,17 +1,6 @@
 <?php
 require 'template/header.php';
 require 'database/settings_db.php';
-
-// $sql = "SELECT * FROM carros";
-// $result = $conn->prepare($sql);
-// $result->execute();
-// $cars = $result->fetchAll(PDO::FETCH_OBJ)
-$sql = "SELECT c.chassi, c.modelo, c.valor_aluguel, c.quilometros_rodados, m.nome AS marca 
-      FROM carros AS c
-      JOIN marcas AS m ON c.id_marca = m.id_marca";
-$result = $conn->prepare($sql);
-$result->execute();
-$cars = $result->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 <div id="body">
@@ -26,26 +15,26 @@ $cars = $result->fetchAll(PDO::FETCH_OBJ);
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                </div>
                <div class="modal-body">
-                  <form action="" method="get" class="row g-3">
+                  <form action="verifications/cars_add.php" method="get" class="row g-3">
                      <div class="col-md-6">
                         <label for="inputChassi" class="form-label fw-semibold">Nº do Chassi</label>
-                        <input type="number" class="form-control" id="inputChassi" name="chassi">
+                        <input type="text" maxlength="17" class="form-control" id="inputChassi" name="chassi" required>
                      </div>
                      <div class="col-md-6">
                         <label for="inputValor" class="form-label fw-semibold">Valor do Aluguel</label>
-                        <input type="number" class="form-control" id="inputValor" name="valor">
+                        <input type="number" class="form-control" id="inputValor" name="valor" required>
                      </div>
                      <div class="col-12">
                         <label for="inputModelo" class="form-label fw-semibold">Modelo do Carro</label>
-                        <input type="text" class="form-control" id="inputModelo" name="modelo">
+                        <input type="text" class="form-control" id="inputModelo" name="modelo" required>
                      </div>
                      <div class="col-md-6">
                         <label for="inputKm" class="form-label fw-semibold">Quilômetros Rodados</label>
-                        <input type="number" class="form-control" id="inputKm" name="km">
+                        <input type="text" class="form-control" id="inputKm" name="km" value="Zero">
                      </div>
                      <div class="col-md-6">
                         <label for="inputMarca" class="form-label fw-semibold">Marca do carro</label>
-                        <select id="inputMarca" class="form-select" name="marca">
+                        <select id="inputMarca" class="form-select" name="marca" required>
                            <option selected></option>
                            <?php
                            $sql = "SELECT * FROM marcas";
@@ -54,51 +43,61 @@ $cars = $result->fetchAll(PDO::FETCH_OBJ);
                            $options = $result->fetchAll(PDO::FETCH_OBJ);
                            if ($result->rowCount() > 0) {
                               foreach ($options as $option) {
-                                 echo "<option>$option->nome</option>";
+                                 echo "<option value='$option->id_marca'>$option->nome</option>";
                               }
                            }
                            ?>
                         </select>
                      </div>
+                     <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                        <input type="submit" class="btn btn-primary" value="Adicionar">
+                     </div>
                   </form>
                </div>
-               <div class="modal-footer">
-                  <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                  <button type="button" class="btn btn-primary">Adicionar</button>
-               </div>
+               </form>
             </div>
          </div>
       </div>
    </div>
+   <?php
+   
+   ?>
    <div class="container-fluid py-4 table-responsive">
       <table class="table table-striped table-bordered">
          <thead>
             <tr class="table-dark">
-               <th>Nº do Chassi</th>
-               <th>Modelo</th>
-               <th>Valor do Aluguel (R$)</th>
-               <th>Quilômetros Rodados (Km)</th>
-               <th>Marca</th>
-               <th>Ações</th>
+               <th class="text-center align-content-center">Nº do Chassi</th>
+               <th class="text-center align-content-center">Modelo</th>
+               <th class="text-center align-content-center">Valor do Aluguel (R$)</th>
+               <th class="text-center align-content-center">Quilômetros Rodados (Km)</th>
+               <th class="text-center align-content-center">Marca</th>
+               <th class="text-center align-content-center">Ações</th>
             </tr>
          </thead>
          <tbody>
             <?php
+            $sql = "SELECT c.chassi, c.modelo, c.valor_aluguel, c.quilometros_rodados, m.nome AS marca 
+                  FROM carros AS c
+                  JOIN marcas AS m ON c.id_marca = m.id_marca";
+            $result = $conn->prepare($sql);
+            $result->execute();
+            $cars = $result->fetchAll(PDO::FETCH_OBJ);
+
             if($result->rowCount() > 0) {
                foreach ($cars as $car) {
                   echo '<tr>';
-                  echo "<th> $car->chassi </th>";
-                  echo "<td> $car->modelo </td>";
-                  echo "<td> R$ $car->valor_aluguel,00 </td>";
-                  echo "<td> $car->quilometros_rodados Km </td>";
-                  echo "<td> $car->marca </td>";
-                  echo '<th>';
-                  echo '<button class="btn bg-warning">Editar</button>';
-                  echo '<button class="btn bg-danger">Deletar</button>';
+                  echo "<th class='text-center align-content-center'> $car->chassi </th>";
+                  echo "<td class='text-center align-content-center'> $car->modelo </td>";
+                  echo "<td class='text-center align-content-center'> R$ $car->valor_aluguel,00 </td>";
+                  echo "<td class='text-center align-content-center'> $car->quilometros_rodados Km </td>";
+                  echo "<td class='text-center align-content-center'> $car->marca </td>";
+                  echo '<th class="align-content-center d-flex justify-content-center">';
+                  echo '<button class="btn bg-warning me-2">Editar</button>';
+                  echo '<button class="btn bg-danger ms-2">Deletar</button>';
                   echo '</th>';
                   echo '</tr>';
                }
-               $content = true;  
             } else {
                $content = false;
             }
@@ -106,7 +105,7 @@ $cars = $result->fetchAll(PDO::FETCH_OBJ);
          </tbody>
       </table>
       <?php
-      if ($content == false) {
+      if (isset($content)) {
          echo '<div class="container-fluid d-flex justify-content-center py-3">
                   <div class="card text-danger border-2">
                      <div class="card-body">
--- site/verifications/cars_add.php
+++ site/verifications/cars_add.php
@@ -1,1 +1,19 @@
 <?php
+require '../database/settings_db.php';
+if (isset($_GET['chassi']) && !empty($_GET['chassi']) && isset($_GET['valor']) && !empty($_GET['valor']) && isset($_GET['modelo']) && !empty($_GET['modelo']) && isset($_GET['km']) && !empty($_GET['km']) && isset($_GET['marca']) && !empty($_GET['marca'])) {
+    $chassi = $_GET['chassi'];
+    $valor = $_GET['valor'];
+    $modelo = $_GET['modelo'];
+    $km = $_GET['km'];
+    $marca = $_GET['marca'];
+
+    $sql = "INSERT INTO carros(chassi, modelo, valor_aluguel, quilometros_rodados, id_marca) VALUES(:chassi, :modelo, :valor, :km, :marca)";
+    $result = $conn->prepare($sql);
+    $result->bindValue("chassi", $chassi);
+    $result->bindValue("modelo", $modelo);
+    $result->bindValue("valor", $valor);
+    $result->bindValue("km", $km);
+    $result->bindValue("marca", $marca);
+    $result->execute();
+    header('Location: ../cars_table.php?register="ok"');
+}
